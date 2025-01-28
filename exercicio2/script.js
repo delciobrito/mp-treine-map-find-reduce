983dffc8731ab1ad3409636a314ff32f026edbf0
Delcio Brito <delciobrito82@gmail.com>
2025-01-28 02:20:33 -0300

    Creating variables and functions

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,27 @@
-import {drinks} from '../data.js'
+import { drinks } from "../data.js";
+
+const fruits = document.querySelector("#fruta");
+const distilleds = document.querySelector("#destilado");
+const buttonDrinksCreator = document.querySelector("#criarDrink");
+const drink = document.querySelector("#resultado");
+
+buttonDrinksCreator.addEventListener("click", () => {
+  let fruit = fruits.value;
+  let distilled = distilleds.value;
+
+  drinksCreator(fruit, distilled);
+});
+
+function drinksCreator(fruit, distilled) {
+  if (fruit != "" && distilled != "") {
+    drink.innerHTML = `
+                        <h1>Bebida pronta<h1/>
+                    `;
+  } else {
+    drink.innerHTML = `
+                        <h1>Falta um ingrediente<h1/>
+                    `;
+  }
+}
 
 console.log(drinks);
